7900f1bbef2cead746cd148cce8eaa106e88fa05
davidomole <thedavidomole@gmail.com>
2021-09-11 23:36:32 +0100

    :heavy_plus_sign: install firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRDR7lnW-VCnFX_IXn47mA9EOJaK605Rk",
+    authDomain: "instagram-ish-7e08d.firebaseapp.com",
+    projectId: "instagram-ish-7e08d",
+    storageBucket: "instagram-ish-7e08d.appspot.com",
+    messagingSenderId: "316099777689",
+    appId: "1:316099777689:web:9017381a17df9e3eb69408",
+    databaseUrl: "https://instagram-ish-7e08d-default-rtdb.firebaseio.com/"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
